1be4339ef17d52cc3d47de05fd64ae0155f68936
Julia <ujilia@gmail.com>
2017-03-27 23:09:53 +0200

    Modified test for login

--- test/test_login.py
+++ test/test_login.py
@@ -1,4 +1,7 @@
 
 def test_login(app):
-    app.session.login("administrator", "root")
+    # if app.session.is_logged_in_as == "administrator":
+    #     app.session.logout()
+    # app.session.login("administrator", "root")
+    print(app.session.get_logged_user())
     assert app.session.is_logged_in_as("administrator")
