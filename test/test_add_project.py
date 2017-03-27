86bca8ab7ab3a6ce3dadf80c536e5ee13e5042b2
Julia <ujilia@gmail.com>
2017-03-27 23:12:37 +0200

    Modified tests for projects

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -16,9 +16,18 @@ testdata = [
 
 @pytest.mark.parametrize("project", testdata, ids=[repr(x) for x in testdata])
 def test_add_project(app, project):
-    old_projects = app.project.get_project_list()
+    old_projects = app.soap.get_project_list("administrator", "root")
     app.project.create_project(project)
-    assert len(old_projects) + 1 == app.project.count()
-    new_projects = app.project.get_project_list()
+    new_projects = app.soap.get_project_list("administrator", "root")
     old_projects.append(project)
     assert sorted(old_projects, key=Project.get_name) == sorted(new_projects, key=Project.get_name)
+
+
+# @pytest.mark.parametrize("project", testdata, ids=[repr(x) for x in testdata])
+# def test_add_project(app, project):
+#     old_projects = app.project.get_project_list()
+#     app.project.create_project(project)
+#     assert len(old_projects) + 1 == app.project.count()
+#     new_projects = app.project.get_project_list()
+#     old_projects.append(project)
+#     assert sorted(old_projects, key=Project.get_name) == sorted(new_projects, key=Project.get_name)
--- test/test_del_project.py
+++ test/test_del_project.py
@@ -3,12 +3,23 @@ import random
 
 
 def test_delete_project(app):
-    if len(app.project.get_project_list()) == 0:
+    if len(app.soap.get_project_list("administrator", "root")) == 0:
         app.project.create_project(Project(name="something new", description="something interesting"))
-    old_projects = app.project.get_project_list()
+    old_projects = app.soap.get_project_list("administrator", "root")
     project = random.choice(old_projects)
     app.project.delete_project(project)
-    new_projects = app.project.get_project_list()
-    assert len(old_projects) - 1 == app.project.count()
+    new_projects = app.soap.get_project_list("administrator", "root")
     old_projects.remove(project)
     assert sorted(old_projects, key=Project.get_name) == sorted(new_projects, key=Project.get_name)
+
+
+# def test_delete_project(app):
+#     if len(app.project.get_project_list()) == 0:
+#         app.project.create_project(Project(name="something new", description="something interesting"))
+#     old_projects = app.project.get_project_list()
+#     project = random.choice(old_projects)
+#     app.project.delete_project(project)
+#     new_projects = app.project.get_project_list()
+#     assert len(old_projects) - 1 == app.project.count()
+#     old_projects.remove(project)
+#     assert sorted(old_projects, key=Project.get_name) == sorted(new_projects, key=Project.get_name)
